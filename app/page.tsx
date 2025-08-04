ddcb97b79ad7118a798b6444a6c0ab9f8e78edec
Zerotix <erfan.habibi6.79@gmail.com>
2025-08-04 19:04:52 +0330

    delete components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import Button from "@/components/botton/Botton";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="w-full h-screen flex items-center justify-center flex-col gap-4">
       <h1 className="font-semibold text-2xl">NextJs Middleware Bypass</h1>
       <h2 className="font-semibold text-xl">In dashboard</h2>
-      <Button model="primary" text="ورود به صفحه محافظت شده" cursorType="pointer" href="/protected-route" />
+      <Link href={"/protected-route"}>
+        <button className="h-[42px] px-6 cursor-pointer rounded-3xl inline-flex items-center justify-center bg-blue-600 hover:bg-blue-500 text-white text-lg p-2 transition-all">
+          ورود به صفحه محافظت شده
+        </button>
+      </Link>
     </div>
   );
 }
--- app/protected-route/page.tsx
+++ app/protected-route/page.tsx
@@ -1,10 +1,14 @@
-import Button from "@/components/botton/Botton";
+import Link from "next/link";
 
 const ProtectedRoute = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center flex-col gap-4">
       <h1 className="font-semibold text-2xl">In Protected Route</h1>
-      <Button model="primary" text="بازگشت به داشبورد" cursorType="pointer" href="/" />
+      <Link href={"/"}>
+        <button className="h-[42px] px-6 cursor-pointer rounded-3xl inline-flex items-center justify-center bg-blue-600 hover:bg-blue-500 text-white text-lg p-2 transition-all">
+          بازگشت به داشبورد
+        </button>
+      </Link>
     </div>
   );
 };
